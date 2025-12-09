8c115ac3b393ba38ed836cd30ff967f33fda30de
mehran <mehranmohamadi1311@gmail.com>
2025-12-09 14:33:50 +0330

    chore: install tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,9 @@
-
-
 function App() {
-
   return (
-    <h1>
+    <h1 className="py-12 text-center text-3xl text-purple-600">
       Hello, World!
     </h1>
-  )
+  );
 }
 
-export default App
+export default App;
