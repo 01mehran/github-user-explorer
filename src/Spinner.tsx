d49e145b3b8562e643cb9e7b52e4acce59c6b354
mehran <mehranmohamadi1311@gmail.com>
2025-12-12 12:53:22 +0330

    chore: update react react-dom.

--- src/Spinner.tsx
+++ src/Spinner.tsx
@@ -3,7 +3,7 @@ import { Riple } from 'react-loading-indicators';
 export default function Spinner() {
   return (
     <div className="absolute top-1/2 left-1/2 -translate-x-1/2 transform">
-      <Riple color="#32cd32" size="small" text="" textColor="" />
+      <Riple color="#32cd32" size="medium" text="" textColor="" />
     </div>
   );
 }
